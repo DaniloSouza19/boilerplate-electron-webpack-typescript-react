8e590e70486d6057709a0cb3220b3f789287d089
Danilo Souza <danilo_souza19@outlook.com>
2021-06-21 17:35:55 -0300

    add enviroment variables

--- src/api/config/oracleConfig.ts
+++ src/api/config/oracleConfig.ts
@@ -5,9 +5,9 @@ import {
 } from 'oracledb';
 
 export const connectionOptions: ConnectionAttributes = {
-  connectString: '',
-  user: '',
-  password: '',
+  connectString: process.env.CONNECT_STRING_DB,
+  user: process.env.USER_DB,
+  password: process.env.PASSWORD_DB,
 };
 
 export const executeOptions: ExecuteOptions = {
